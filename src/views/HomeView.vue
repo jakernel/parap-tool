413c5ef4d7a8b6746c3d0b5384ad9092949e3825
pzx521521 <pzx521521@qq.com>
2025-01-16 17:44:24 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,7 +16,7 @@ const showNav = !CUSTOMPARAMS.singlefile
         <router-link to="/html-to-apk">HtmlToApk</router-link>|
         <router-link to="/html-to-exe">HtmlToExe</router-link>|
         <router-link to="/qr-merge">QR Merge</router-link>|
-        <a href="https://wallpaper.parap.us.kg">wallpaper</a>
+        <a href="https://parap.us.kg">wallpaper</a>
       </div>
     </template>
     <div>
